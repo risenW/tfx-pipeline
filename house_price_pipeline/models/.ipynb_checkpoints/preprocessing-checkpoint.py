23111e1dea437c922308569ff3be1387435e17fe
risenW <risingodegua@gmail.com>
2020-09-06 16:49:17 +0000

    Reinitialized git

---
+++ house_price_pipeline/models/.ipynb_checkpoints/preprocessing-checkpoint.py
@@ -0,0 +1,41 @@
+"""TFX House Pricing preprocessing.
+
+This file defines a template for TFX Transform component.
+"""
+
+import tensorflow as tf
+import tensorflow_transform as tft
+
+from models import features
+
+def preprocessing_fn(inputs):
+    """tf.transform's callback function for preprocessing inputs.
+    Args:
+    inputs: map from feature keys to raw not-yet-transformed features.
+
+    Returns:
+    Map from string feature key to transformed feature operations.
+    """
+    outputs = {}
+    
+    for key in features.DENSE_FLOAT_FEATURE_KEYS:
+        outputs[features.transformed_name[key]] = tft.scale_to_z_score(inputs[key])
+        
+    for key in features.VOCAB_FEATURE_KEYS:
+        # Build a vocabulary for this feature.
+        outputs[features.transformed_name[key]] = tft.compute_and_apply_vocabulary(
+        inputs[key],
+        top_k=features.VOCAB_SIZE,
+        num_oov_buckets=features.OOV_SIZE)
+        
+    for key, num_buckets in zip(features.BUCKET_FEATURE_KEYS,
+                              features.BUCKET_FEATURE_BUCKET_COUNT):
+        outputs[features.transformed_name[key]] = tft.bucketize(inputs[key], num_buckets)
+    
+    outputs[features.transformed_name(features.LABEL_KEY)] = inputs[features.LABEL_KEY]
+    
+    return outputs
+        
+    
+
+    
---
+++ house_price_pipeline/pipeline/.ipynb_checkpoints/configs-checkpoint.py
@@ -0,0 +1,35 @@
+"""TFX House Pricing Pipeline Config.
+
+This file defines environments for a TFX taxi pipeline.
+"""
+
+import os
+
+
+PIPELINE_NAME = 'house_price_pipeline'
+
+# Retrieve your GCP project. You can choose which project
+# to use by setting GOOGLE_CLOUD_PROJECT environment variable.
+try:
+  import google.auth 
+  try:
+    _, GOOGLE_CLOUD_PROJECT = google.auth.default()
+  except google.auth.exceptions.DefaultCredentialsError:
+    GOOGLE_CLOUD_PROJECT = ''
+except ImportError:
+  GOOGLE_CLOUD_PROJECT = ''
+
+
+# Specify your GCS bucket name here. You have to use GCS to store output files
+GCS_BUCKET_NAME = GOOGLE_CLOUD_PROJECT + '-kubeflowpipelines-default'
+
+PREPROCESSING_FN = '/home/jupyter/imported/house_price_pipeline/models.preprocessing/preprocessing_fn'
+RUN_FN = 'models.model.run_fn'
+
+TRAIN_NUM_STEPS = 1000
+EVAL_NUM_STEPS = 150
+
+EVAL_ACCURACY_THRESHOLD = 0.8
+
+
+
